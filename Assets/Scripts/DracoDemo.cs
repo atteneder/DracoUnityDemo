5a8d811ca4d9d1539ad56f0744ecf19700886d9a
Andreas Atteneder <andreas.atteneder@gmail.com>
2021-05-26 16:45:57 +0200

    feat: Adding minimalistic demo

---
+++ Assets/Scripts/DracoDemo.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using UnityEngine;
+using Draco;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class DracoDemo : MonoBehaviour {
+    
+    public string filePath;
+
+    async void Start() {
+        
+        // Load file into memory
+        var fullPath = Path.Combine(Application.streamingAssetsPath, filePath);
+        var data = File.ReadAllBytes(fullPath);
+        
+        // Convert data to Unity mesh
+        var draco = new DracoMeshLoader();
+        // Async decoding has to start on the main thread and spawns multiple C# jobs.
+        var mesh = await draco.ConvertDracoMeshToUnity(data);
+        
+        if (mesh != null) {
+            // Use the resulting mesh
+            GetComponent<MeshFilter>().mesh= mesh;
+        }
+    }
+}
