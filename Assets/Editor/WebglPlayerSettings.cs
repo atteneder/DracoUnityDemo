19593ef0e55e6c28e08f6bb5b2010acaaaf389ee
Andreas Atteneder <andreas.atteneder@gmail.com>
2023-06-15 12:12:57 +0200

    feat: Added menu item for enabling WebGL multithreading

---
+++ Assets/Editor/WebglPlayerSettings.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEngine;
+
+public static class WebglPlayerSettings
+{
+    [MenuItem("Tools/Set WebGL Settings")]
+    static void WebglSet()
+    {
+        PlayerSettings.WebGL.threadsSupport = true;
+    }
+    
+    [MenuItem("Tools/Unset WebGL Settings")]
+    static void WebglUnSet()
+    {
+        PlayerSettings.WebGL.threadsSupport = false;
+    }
+}
